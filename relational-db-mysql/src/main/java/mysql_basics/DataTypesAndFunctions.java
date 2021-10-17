f15e8941a356ced64a9120ad43f33d037fe5ca26
Tong Chen <tong.chen@etu.utc.fr>
2021-10-17 16:25:37 +0200

    reset relational db mysql

--- relational-db-mysql/src/main/java/mysql_basics/DataTypesAndFunctions.java
+++ relational-db-mysql/src/main/java/mysql_basics/DataTypesAndFunctions.java
@@ -1,9 +1,6 @@
 package mysql_basics;
 
-// MySQL数据库：
-// 1. Oracle旗下免费DB
-// 2. MySQL数据实际存储在安装目录下/data/数据库名称/...(一个库对于一个文件夹)
-public class BaseMysql {
+public class DataTypesAndFunctions {
 
     // 基本数据类型的理解
     // 1. Float、Decimal 存储金额的区别？
@@ -35,4 +32,35 @@ public class BaseMysql {
     //    从一张表(驱动表)循环读行，然后根据关联字段从另外一张表(被驱动表)提取满足的行，然后取出两个表的结果合集
     // 2. Block Nested Loop Join
     //    把原始驱动表的数据读取到join_buffer中，然后扫描被驱动表，从被驱动表中提取每一行和join_buffer中的数据对比
+
+    /* 从一个表中提取数据到另外一个表
+     * 1. SELECT names
+     *    INTO   new_table_name  在默认的file-group位置创建一个新表
+     *    FROM   old_table_name
+     * 2. INSERT INTO new_table_name
+     *    SELECT names           选择出来的列需要和目标新表匹配
+     *    FROM old_table_name
+     */
+
+    /* 如果修改已经建好的Table
+     * 1. ALTER old_table_name
+     *    ADD   column_name datatype;
+     * 2. ALTER old_table_name
+     *    ALTER COLUMN column_name datatype;
+     */
+
+    /* 如何删除已经建好的表
+     * 1. DROP TABLE old_table_name;     将数据和表一同删除
+     * 2. TRUNCATE TABLE old_table_name; 只清空表中的全部数据，并且只写入一次日志信息
+     */
+
+    /* 创建View视图，并从视图中查询数据: 视图数据的变化和原表数据的变化相互影响
+     * CREATE VIEW view_name AS
+     * SELECT column1, column2 ,,,
+     * FROM old_table_name;
+     *
+     * SELECT * FROM view_name; 视图是基于SQL语句结果表上的一个虚拟表
+     * ALTER VIEW view_name;    修改视图
+     * DROP VIEW view_name;     删除视图
+     */
 }
