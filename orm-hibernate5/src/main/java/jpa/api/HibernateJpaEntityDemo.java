51c5e232a90dc2c9d89483d5544ead6f0ecc5cea
tchen <tchen@murex.com>
2022-06-22 18:18:56 +0200

    refresh project

--- orm-hibernate5/src/main/java/jpa/api/HibernateJpaEntityDemo.java
+++ orm-hibernate5/src/main/java/jpa/api/HibernateJpaEntityDemo.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 // TODO. Hibernate Jpa annotation Mapping映射问题
 // 1. @Entity() 如果没有设置名称，使用默认的类型名称
-// 2. @Entity(name = "entity-name") 如果设置名称为全路径，则必须使用相同的值 ?
+// 2. @Entity(name = "entity-name") 如果设置名称为全路径，则必须使用全路径来查询 !!
 public class HibernateJpaEntityDemo {
 
     static StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
