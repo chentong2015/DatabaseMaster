b6be80fd8bdc1c98605a6a11fca06ed732471a7b
tchen <tchen@murex.com>
2023-06-01 18:12:19 +0200

    update hibernate dialect resolver

---
+++ orm-jpa-hibernate/native-hibernate6-api-master/src/main/java/dialect/DialectResolverTemplate.java
@@ -0,0 +1,47 @@
+package dialect;
+
+import org.hibernate.dialect.Dialect;
+import org.hibernate.engine.jdbc.dialect.internal.StandardDialectResolver;
+import org.hibernate.engine.jdbc.dialect.spi.DialectResolutionInfo;
+import org.hibernate.engine.jdbc.dialect.spi.DialectResolver;
+
+// 使用自定义的Dialect来做判断
+public class CustomDialectResolver implements DialectResolver {
+
+    private static final long serialVersionUID = 3438546111602228772L;
+    private static final int ORACLE_8 = 8;
+    private static final int ORACLE_9 = 9;
+    private static final int SYBASE_11 = 11;
+    private static final int SYBASE_12 = 12;
+
+    @Override
+    public Dialect resolveDialect(DialectResolutionInfo info) {
+        String databaseName = info.getDatabaseName();
+        int databaseMajorVersion = info.getDatabaseMajorVersion();
+        /*
+        if ("Adaptive Server Enterprise".equals(databaseName)) {
+            return switch (databaseMajorVersion) {
+                case SYBASE_11 -> new Sybase11Dialect();
+                case SYBASE_12 -> new Sybase12Dialect();
+                default -> new Sybase15Dialect();
+            };
+        }
+        if ("Oracle".equals(databaseName)) {
+            return switch (databaseMajorVersion) {
+                case ORACLE_8 -> new Oracle8Dialect();
+                case ORACLE_9 -> new Oracle9Dialect();
+                default -> new Oracle10Dialect();
+            };
+        }
+        if (databaseName.startsWith("Microsoft SQL Server")) {
+            return new SQLServer2012Dialect();
+        }
+        if ("PostgreSQL".equals(databaseName)) {
+            return new MxPostgreSQL95Dialect();
+        } */
+
+        //Default Case
+        final StandardDialectResolver standardDialectResolver = new StandardDialectResolver();
+        return standardDialectResolver.resolveDialect(info);
+    }
+}
