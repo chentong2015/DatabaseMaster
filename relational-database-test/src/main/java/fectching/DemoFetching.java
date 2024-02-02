bcdc0529f8fd85c50144cc86d88ad7c55aacfd42
chen tong <chen2015tong@gmail.com>
2024-02-02 15:09:51 +0100

    update project and add db migration

---
+++ relational-database-test/src/main/java/fectching/DemoFetching.java
@@ -0,0 +1,26 @@
+package fectching;
+
+import java.sql.*;
+
+// Fetching 抓取数据
+// The process of grabbing data from the db and making it available to the application.
+// - Fetching too much data adds overhead in terms of both JDBC communication and ResultSet processing.
+// - Fetching too little data might cause additional fetching to be needed.
+
+// TODO. Fetching一般分成两种: Lazy & Eager
+//  Lazy Fetch doesn't work outside transaction
+//  在事务之外的延迟获取可能造成timeout(如果返回Stream<>且数据量较大)
+public class DemoFetching {
+
+    // 如果设置AutoCommit=false, 则不能设置fetch size的参数
+    public void testFetchSize() throws SQLException {
+        Connection connection = DriverManager.getConnection("url");
+        connection.setAutoCommit(false);
+
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("query");
+        resultSet.setFetchSize(50);
+
+        ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+    }
+}
---
+++ relational-database-test/src/main/java/metadata/MyDatabaseType.java
@@ -0,0 +1,12 @@
+package metadata;
+
+public enum MyDatabaseType {
+
+    SYBASE,
+    ORACLE,
+    MS_SQL_SERVER,
+    POSTGRES,
+    HSQL,
+    HSQL_SERVER,
+    DERBY;
+}
