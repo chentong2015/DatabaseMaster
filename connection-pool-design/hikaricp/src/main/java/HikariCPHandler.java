df2dc6402ae593bfc6e11f062a447c5c79801987
tchen <tong.chen@lexisnexisrisk.com>
2024-07-21 18:18:25 +0200

    update base cp design with project

--- connection-pool-design/hikaricp/src/main/java/HikariCPHandler.java
+++ connection-pool-design/hikaricp/src/main/java/HikariCPHandler.java
@@ -20,17 +20,30 @@ public class HikariCPHandler {
         dataSource.setMaximumPoolSize(threads * 2 + 1);
 
         // 最少闲置状态的连接，保证连接的基数(CP将维持这个数量)，默认10
+        // minIdle==maxPoolSize 将会造成Fixed size Pool固定大小的池
         dataSource.setMinimumIdle(threads);
 
-        // 设置等待获取连的超时时间，在Connection不够的情况下很重要，默认30s
-        dataSource.setConnectionTimeout(30000);
-
-        // 连接的最大存活时间，超过生命周期的连接(没有正在被使用)将被移除
-        dataSource.setMaxLifetime(580000);
+        // 设置等待获取连的超时时间，以支持可能耗时较长的操作，默认30s
+        // 当Connection数量不足时，即使提高Timeout等待时间，仍然可能造成异常 !!
+        dataSource.setConnectionTimeout(60000);
 
         // 处于闲置状态的Connection的超时时间, 默认10m
         // 如果这个参数接近或者超过MaxLifetime最大生命周期，则被Disable
         dataSource.setIdleTimeout(500000);
+
+        // TODO. MaxLifetime参数可能造成异常，如何模拟被DB超时 ?
+        // 连接的最大存活时间，超过生命周期的连接(没有在被使用的连接)将被移除
+        // it should be several seconds shorter than any database or infrastructure imposed connection time limit.
+        dataSource.setMaxLifetime(580000);
+
+        // TODO. 监测空闲的连接是否还有效，没有被超时关闭 !
+        // 尝试保持连接活动的频率，以防止数据库或网络基础设施超时
+        // 此值必须小于maxLifetime值, keepalive只会发生在空闲连接上
+        // 当针对给定连接的keepalive时间到达时，该连接将从池中移除，然后“ping”，然后返回到池中
+        dataSource.setKeepaliveTime(50000);
+
+        // Pool will wait for a connection to be validated as alive.
+        // dataSource.setValidationTimeout(3000);
 
         // TODO. 判断CP中获取的连接是否有效，注意连接泄露
         try (Connection connection = dataSource.getConnection()) {
