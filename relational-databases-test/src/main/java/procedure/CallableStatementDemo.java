339bf9f80cc333d9871937a95fc34686a8d12887
tchen <tong.chen@lexisnexisrisk.com>
2024-09-17 17:55:52 +0200

    refresh rebase project

--- relational-databases-test/src/main/java/procedure/CallableStatementDemo.java
+++ relational-databases-test/src/main/java/procedure/CallableStatementDemo.java
@@ -1,10 +1,11 @@
 package procedure;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Types;
 
-public class CallableStatement {
+public class CallableStatementDemo {
 
     // 通过connection.prepareCall()调用DB中预先定义好的Procedure Function
     // 1. 定义传递的参数
@@ -13,7 +14,7 @@ public class CallableStatement {
     // The CallableStatement object provides methods for setting up its IN and OUT parameters,
     // and methods for executing the call to a stored procedure.
     public long testCallableStatement(Connection connection, String field1) {
-        try (java.sql.CallableStatement statement = connection.prepareCall("{call sp_getUniqueId(?, ?, ?, ?)}")) {
+        try (CallableStatement statement = connection.prepareCall("{call sp_getUniqueId(?, ?, ?, ?)}")) {
             statement.setString(1, field1);
             statement.registerOutParameter(2, Types.BIGINT);
             statement.execute();
--- relational-databases/sql-server/src/main/java/DemoSqlServerJDBC.java
+++ relational-databases/sql-server/src/main/java/DemoSqlServerJDBC.java
@@ -1,22 +1,28 @@
 import java.math.BigDecimal;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DemoSqlServerJDBC {
 
     public static void main(String[] args) throws SQLException {
+        String url = "jdbc:sqlserver://server:1433;instanceName=MSSQLSERVER;databaseName=QA_FMM412_WJI";
+        Connection connection = DriverManager.getConnection(url, "Q_WJI", "Hello00");
+        String query = "create table test(id_num int, username nvarchar(100));";
+
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(query);
+
+        connection.close();
+        System.out.println("Finish");
+    }
+
+    // TODO. PreparedStatement替换的参数实际值需要满足类型匹配
+    public static void testMain() throws SQLException {
         String url = "jdbc:sqlserver://server:1433;databaseName=DAS_CONV_TOOL;trustServerCertificate=true";
         Connection connection = DriverManager.getConnection(url, "INSTAL", "INSTALL");
         String query = "insert into test_table (code, value1, value2) values (?, ?, ?)";
 
         PreparedStatement preparedStatement = connection.prepareStatement(query);
         preparedStatement.setInt(1, 4);
-
-        // TODO. 如果数据库中的Table Column Type定义为Numeric, 使用Java String类型插入数据则会报错
-        // ERROR: column "sc_actif" is of type numeric but expression is of type character varying
-
         // preparedStatement.setString(2, "20");
         // preparedStatement.setString(3, "7");
         preparedStatement.setBigDecimal(2, BigDecimal.valueOf(21));
@@ -24,7 +30,6 @@ public class DemoSqlServerJDBC {
 
         int rows = preparedStatement.executeUpdate();
         System.out.println(rows);
-
         preparedStatement.close();
         connection.close();
     }
